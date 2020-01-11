9c8912adc1adb10e2fbc76aa373536987999a408
Freya19 <freya0319@126.com>
2020-01-11 10:27:08 +0800

    3 files-delNode and java bases

---
+++ src/main/java/freya19/practice/Java_Base/test_int_Integer.java
@@ -0,0 +1,13 @@
+package freya19.practice.Java_Base;
+
+public class test_int_Integer {
+    public static void main(String[] args) {
+        Integer a=200;
+        Integer b=200;
+        Integer a1=2;
+        Integer b1=2;
+        System.out.println(a==b); //false   1
+        System.out.println(a1==b1); //true  2
+    }
+
+}
