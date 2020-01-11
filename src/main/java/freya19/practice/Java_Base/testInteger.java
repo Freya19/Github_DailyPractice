7c625f1e2757372c3c2ab1cd54620e04cdcfc10f
Gepeng18 <robin_ge@foxmail.com>
2020-01-11 18:38:24 +0800

    Integer

--- src/main/java/freya19/practice/Java_Base/testInteger.java
+++ src/main/java/freya19/practice/Java_Base/testInteger.java
@@ -2,17 +2,19 @@ package freya19.practice.Java_Base;
 
 public class testInteger {
     public static void main(String[] args) {
+
         int i=0;
-        int j =2;
-
-
+        //i.
+        Integer b = 0;
+        //b.compareTo()
         //-128 127
         Integer integer = 5;
-        int i1 = integer.compareTo(6);
-        int i2 = integer.compareTo(5);
-        System.out.println(i1);
-        System.out.println(i2);
+        Integer c = 6;
+        Integer d = 6;
+        System.out.println(c);
+        System.out.println(d);
         Character c1;
+        Double e = 9.0;
 
 
 
