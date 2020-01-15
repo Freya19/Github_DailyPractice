f6ca35126409552ecb5bc05018d268869b941cf7
Gepeng18 <robin_ge@foxmail.com>
2020-01-15 15:23:03 +0800

    add -3

---
+++ src/main/java/gepeng18/submit/移除元素L27.java
@@ -0,0 +1,66 @@
+package gepeng18.submit;
+
+/**
+ * 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+ * <p>
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ * <p>
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 给定 nums = [3,2,2,3], val = 3,
+ * <p>
+ * 函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+ * <p>
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * <p>
+ * 示例 2:
+ * <p>
+ * 给定 nums = [0,1,2,2,3,0,4,2], val = 2,
+ * <p>
+ * 函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+ * <p>
+ * 注意这五个元素可为任意顺序。
+ * <p>
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-element
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+
+public class 移除元素L27 {
+    public static void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int left = 0, right = nums.length - 1;
+        if(left == right){
+            if(nums[left] == val){
+                return 0;
+            }else{
+                return 1;
+            }
+        }
+        while (left < right) {
+            while (left<nums.length && nums[left] != val )
+                left++;
+
+            while (right>=0 && nums[right] == val)
+                right--;
+            if (left < right)
+                swap(nums, left, right);
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int result = removeElement(new int[]{2}, 3);
+        System.out.println(result);
+    }
+}
