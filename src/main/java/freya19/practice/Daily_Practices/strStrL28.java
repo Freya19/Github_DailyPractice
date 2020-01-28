8bd8cc3cd0c59b24d963ac678f5a878278933b5b
Freya19 <freya0319@126.com>
2020-01-28 23:54:16 +0800

    Queue的使用及总结

--- src/main/java/freya19/practice/Daily_Practices/strStrL28.java
+++ src/main/java/freya19/practice/Daily_Practices/strStrL28.java
@@ -75,3 +75,57 @@ public class strStrL28 {
         System.out.println(ss.strStr(s9,s10)); // 输出是0 。。。  这是为啥嘞？
     }
 }
+
+
+/*
+有关队列：Queue
+队列是一种特殊的线性表，它只允许在表的前端进行删除操作，而在表的后端进行插入操作。
+LinkedList类实现了Queue接口，因此我们可以把LinkedList当成Queue来用。
+
+示例：
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+        //add()和remove()方法在失败的时候会抛出异常(不推荐)
+        Queue<String> queue = new LinkedList<String>();
+        //添加元素
+        queue.offer("a");
+        queue.offer("b");
+        queue.offer("c");
+        queue.offer("d");
+        queue.offer("e");
+        for(String q : queue){
+            System.out.println(q);
+        }
+        System.out.println("===");
+        System.out.println("poll="+queue.poll()); //返回第一个元素，并在队列中删除
+        for(String q : queue){
+            System.out.println(q);
+        }
+        System.out.println("===");
+        System.out.println("element="+queue.element()); //返回第一个元素
+        for(String q : queue){
+            System.out.println(q);
+        }
+        System.out.println("===");
+        System.out.println("peek="+queue.peek()); //返回第一个元素
+        for(String q : queue){
+            System.out.println(q);
+        }
+    }
+}
+一些方法的区别：
+offer，add 区别：
+一些队列有大小限制，因此如果想在一个满的队列中加入一个新项，多出的项就会被拒绝。
+这时新的 offer 方法就可以起作用了。它不是对调用 add() 方法抛出一个 unchecked 异常，而只是得到由 offer() 返回的 false。
+简言之，offer方法不报异常，不处理异常。
+
+poll，remove 区别：
+remove() 和 poll() 方法都是从队列中删除第一个元素。remove() 的行为与 Collection 接口的版本相似，
+但是新的 poll() 方法在用空集合调用时不是抛出异常，只是返回 null。因此新的方法更适合容易出现异常条件的情况。
+
+peek，element区别：
+element() 和 peek() 用于在队列的头部查询元素。与 remove() 方法类似，在队列为空时， element() 抛出一个异常，而 peek() 返回 null。
+ */
