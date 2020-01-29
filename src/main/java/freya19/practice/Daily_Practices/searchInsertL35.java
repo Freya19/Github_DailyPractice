2c6739665631a504e02135903c5713b84d4a05b3
Freya19 <freya0319@126.com>
2020-01-29 21:04:49 +0800

    索引相同元素，返回索引；没有时，返回插入位置索引

---
+++ src/main/java/freya19/practice/Daily_Practices/searchInsertL35.java
@@ -0,0 +1,44 @@
+/*
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+你可以假设数组中无重复元素。
+
+示例 1:
+输入: [1,3,5,6], 5
+输出: 2
+
+示例 2:
+输入: [1,3,5,6], 2
+输出: 1
+
+示例 3:
+输入: [1,3,5,6], 7
+输出: 4
+
+示例 4:
+输入: [1,3,5,6], 0
+输出: 0
+
+ */
+package freya19.practice.Daily_Practices;
+
+public class searchInsertL35 {
+    public int searchInsert(int[] nums, int target){
+        if(nums==null) return -1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=target) {
+                if(target<nums[0]) return 0;
+                if(target>nums[nums.length-1]) return (nums.length-1);
+                if(nums[i]<target && nums[i+1]>target) return i;
+            }else if(nums[i]==target) return i;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,6};
+        int target = 7;
+
+        searchInsertL35 si = new searchInsertL35();
+        System.out.println(si.searchInsert(arr,target));
+    }
+}
