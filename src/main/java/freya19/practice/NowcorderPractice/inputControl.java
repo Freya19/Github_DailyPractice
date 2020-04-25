e78725b9bfe5fab2c644a18a3a95449bc95dc347
Freya19 <freya0319@126.com>
2020-04-25 23:29:31 +0800

    双指针+二分

---
+++ src/main/java/freya19/practice/NowcorderPractice/inputControl.java
@@ -0,0 +1,19 @@
+package freya19.practice.NowcorderPractice;
+
+import java.util.List;
+
+public class inputControl {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+
+        inputControl inputControl = new inputControl();
+        System.out.println(inputControl.add(1,2));
+    }
+
+
+
+}
