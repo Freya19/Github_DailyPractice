9995fe35a5eefb2dc3ffc97a8b2633355aceecee
Freya19 <freya0319@126.com>
2020-06-01 23:41:33 +0800

    链表

---
+++ src/main/java/freya19/practice/LeetCode/L1431拥有糖果数最多的孩子.java
@@ -0,0 +1,32 @@
+package freya19.practice.LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class L1431拥有糖果数最多的孩子 {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        ArrayList<Boolean> resList = new ArrayList<>();
+
+        int maxCandies=candies[0];
+        for(int candy:candies){
+            maxCandies=Math.max(maxCandies,candy);
+        }
+
+        for(int candy:candies){
+            if(candy+extraCandies>=maxCandies){
+                resList.add(true);
+            } else {
+                resList.add(false);
+            }
+        }
+        return resList;
+    }
+
+    public static void main(String[] args) {
+        int[] candies = {12,1,12};
+        int extra = 10;
+        List<Boolean> list = new L1431拥有糖果数最多的孩子().kidsWithCandies(candies, extra);
+        System.out.println(list);
+    }
+}
