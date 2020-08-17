ff05ee620ac2e6c5abff9bdac8aa5a7a47ec9a31
Gepeng18 <robin_ge@foxmail.com>
2020-08-17 11:16:07 +0800

    增加机体

--- src/main/java/gepeng18/test/Main.java
+++ src/main/java/gepeng18/test/Main.java
@@ -1,10 +1,9 @@
 package gepeng18.test;
 
-import gepeng18.leetcode.ListNode;
-
+import javax.print.DocFlavor;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * class Solution {
@@ -25,24 +24,6 @@ import java.util.Scanner;
  *     }
  * };
  */
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        String[] strings = str.split(" ");
-        int [] rec1 = new int[4];
-        int [] rec2 = new int[4];
-        for (int i=0;i<4;i++){
-            rec1[i] = Integer.valueOf(strings[i]);
-        }
-        for (int i=4;i<8;i++){
-            rec2[i-4] = Integer.valueOf(strings[i]);
-        }
-        boolean b = Math.min(rec1[2], rec2[2]) > Math.max(rec1[0], rec2[0]) &&
-                Math.min(rec1[3], rec2[3]) > Math.max(rec1[1], rec2[1]);
-        if(b)
-            System.out.println(1);
-        else
-            System.out.println(0);
-    }
-}
+
+
+
