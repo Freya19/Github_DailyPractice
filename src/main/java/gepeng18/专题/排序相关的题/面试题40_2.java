7015589ee376566b1c3db6f6571e4d352aeb5744
Gepeng18 <robin_ge@foxmail.com>
2020-10-19 22:06:36 +0800

    -1

--- src/main/java/gepeng18/专题/排序相关的题/面试题40_2.java
+++ src/main/java/gepeng18/专题/排序相关的题/面试题40_2.java
@@ -1,7 +1,9 @@
 package gepeng18.专题.排序相关的题;
 
+import java.util.Arrays;
+
 public class 面试题40_2 {
-    public int[] getLeastNumbers(int[] arr, int k) {
+    public static int[] getLeastNumbers(int[] arr, int k) {
         if (k == 0) {
             return new int[0];
         } else if (arr.length <= k) {
@@ -19,52 +21,39 @@ public class 面试题40_2 {
         return res;
     }
 
-    void partitionArray(int[] arr, int lo, int hi, int k) {
+    private static void partitionArray(int[] arr, int low, int high, int k) {
         // 做一次 partition 操作
-        int m = partition(arr, lo, hi);
+        int m = partition(arr, low, high);
         // 此时数组前 m 个数，就是最小的 m 个数
         if (k == m) {
             // 正好找到最小的 k(m) 个数
             return;
         } else if (k < m) {
             // 最小的 k 个数一定在前 m 个数中，递归划分
-            partitionArray(arr, lo, m-1, k);
+            partitionArray(arr, low, m-1, k);
         } else {
             // 在右侧数组中寻找最小的 k-m 个数
-            partitionArray(arr, m+1, hi, k);
+            partitionArray(arr, m+1, high, k);
         }
     }
 
-    // partition 函数和快速排序中相同，具体可参考快速排序相关的资料
-// 代码参考 Sedgewick 的《算法4》
-    int partition(int[] a, int lo, int hi) {
-        int i = lo;
-        int j = hi + 1;
-        int v = a[lo];
-        while (true) {
-            while (a[++i] < v) {
-                if (i == hi) {
-                    break;
-                }
-            }
-            while (a[--j] > v) {
-                if (j == lo) {
-                    break;
-                }
-            }
-
-            if (i >= j) {
-                break;
-            }
-            swap(a, i, j);
+    private static int partition(int[] arr, int low, int high) {
+        int base = arr[low];
+        int first = low;
+        while (low < high) {
+            while (low < high && arr[high] >= base)
+                high--;
+            while (low < high && arr[low] <= base)
+                low++;
+            if (low < high)
+                swap(arr, low, high);
         }
-        swap(a, lo, j);
-
-        // a[lo .. j-1] <= a[j] <= a[j+1 .. hi]
-        return j;
+        swap(arr, low, first);
+        System.out.println(Arrays.toString(arr));
+        return low;
     }
 
-    void swap(int[] a, int i, int j) {
+    private static void swap(int[] a, int i, int j) {
         int temp = a[i];
         a[i] = a[j];
         a[j] = temp;
